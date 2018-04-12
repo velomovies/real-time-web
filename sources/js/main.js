3712444ac388a5a46b79b3def92119c7bd05aed0
Victor Zumpolle <victor@victorzumpolle.nl>
2018-04-12 17:05:41 +0200

    Keyboard accesibility and styling

--- sources/js/main.js
+++ sources/js/main.js
@@ -1,6 +1,6 @@
 (function () {
 
-  const tones = [{value:'C', color:'white'}, {value:'CIS', color:'black'}, {value:'D', color:'white'}, {value:'DIS', color:'black'}, {value:'E', color:'white'}, {value:'F', color:'white'}, {value:'FIS', color:'black'}, {value:'G', color:'white'}, {value:'GIS', color:'black'}, {value:'A', color:'white'}, {value:'AIS', color:'black'}, {value:'B', color:'white'}, {value:'C2', color:'white'}]
+  const tones = [{value:'C', color:'white', key:'z'}, {value:'CIS', color:'black', key:'s'}, {value:'D', color:'white', key:'x'}, {value:'DIS', color:'black', key:'d'}, {value:'E', color:'white', key:'c'}, {value:'F', color:'white', key:'v'}, {value:'FIS', color:'black', key:'g'}, {value:'G', color:'white', key:'b'}, {value:'GIS', color:'black', key:'h'}, {value:'A', color:'white', key:'n'}, {value:'AIS', color:'black', key:'j'}, {value:'B', color:'white', key:'m'}, {value:'C2', color:'white', key:','}]
 
   const socket = io()
 
@@ -17,19 +17,19 @@
   function antiSpam (e) {
     clearTimeout(function () {
       i = 0
-    }, 1000)
+    }, 500)
    
     i++
 
     setTimeout(function () {
       i = 0
-    }, 1000)
+    }, 500)
     
     if (i > 2) {
       e.target.disabled = true;
       setTimeout(function () {
         e.target.disabled = false;
-      }, 3000)
+      }, 2000)
     }
   }
 
@@ -85,7 +85,7 @@
     let activatedButtons = piano.querySelectorAll('button')
     let pos
     activatedButtons.forEach(function (button) {
-      if(button.innerHTML === tone.value) {
+      if(button.dataset.key === tone.value) {
         pos = button.getBoundingClientRect().x
       }
     })
@@ -99,14 +99,23 @@
 
   tones.forEach(function (tone) {
     const button = document.createElement('button')
+    const keyboard = document.createElement('kbd')
+    const keyboardText = document.createTextNode(tone.key)
     const buttonText = document.createTextNode(tone.value)
 
     tone.user = color
     tone.amount = 0
 
+    keyboard.appendChild(keyboardText) 
     button.appendChild(buttonText)
+    button.appendChild(keyboard)
     button.classList.add(tone.color)
+    button.dataset.key = tone.value
     button.addEventListener('click', function (e) {
+      e.target.classList.add('active')
+      setTimeout(function () {
+        e.target.classList.remove('active')
+      }, 300)
       socket.emit('piano tone', {value: tone.value, user: color})
       startTone(tone)
       pushNote(tone)
@@ -121,15 +130,55 @@
   })
 
   checkBox.addEventListener('change', function(e) {
-    console.log(checkBoxLabel.innerHTML)
-    console.log(e.target.checked)
     if(e.target.checked === true) {
       checkBoxLabel.innerHTML = 'Block mode'
     } else {
       checkBoxLabel.innerHTML = 'Side by side'
+    }   
+  })
+
+  window.addEventListener('keydown', function (e) {
+    switch (e.key) {
+      case 'z':
+        document.querySelector('[data-key="C"]').click()
+      break
+      case 's':
+        document.querySelector('[data-key="CIS"]').click()
+      break
+      case 'x':
+        document.querySelector('[data-key="D"]').click()
+      break
+      case 'd':
+        document.querySelector('[data-key="DIS"]').click()
+      break
+      case 'c':
+        document.querySelector('[data-key="E"]').click()
+      break
+      case 'v':
+        document.querySelector('[data-key="F"]').click()
+      break
+      case 'g':
+        document.querySelector('[data-key="FIS"]').click()
+      break
+      case 'b':
+        document.querySelector('[data-key="G"]').click()
+      break
+      case 'h':
+        document.querySelector('[data-key="GIS"]').click()
+      break
+      case 'n':
+        document.querySelector('[data-key="A"]').click()
+      break
+      case 'j':
+        document.querySelector('[data-key="AIS"]').click()
+      break
+      case 'm':
+        document.querySelector('[data-key="B"]').click()
+      break
+      case ',':
+        document.querySelector('[data-key="C2"]').click()
+      break
     }
-    
-    
   })
 
 }) ()
